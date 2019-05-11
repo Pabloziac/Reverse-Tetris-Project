ab44d0d497d2744e68f7772771a5ce5584cbbba1
Anthony Choi <froshiga@gmail.com>
2019-05-10 20:47:43 -0700

    one line to debug in tetromino

---
+++ Pairs.cpp
@@ -0,0 +1,9 @@
+//
+//  Pairs.cpp
+//  glutapp
+//
+//  Created by Anthony Choi on 5/10/19.
+//  Copyright © 2019 Angelo Kyrilov. All rights reserved.
+//
+
+#include "Pairs.hpp"
---
+++ Pairs.h
@@ -0,0 +1,14 @@
+//
+//  Pairs.hpp
+//  glutapp
+//
+//  Created by Anthony Choi on 5/10/19.
+//  Copyright © 2019 Angelo Kyrilov. All rights reserved.
+//
+
+#ifndef Pairs_hpp
+#define Pairs_hpp
+
+#include <stdio.h>
+
+#endif /* Pairs_hpp */
